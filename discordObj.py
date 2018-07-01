50d58ad1d9445d1b9332c17dce586b8d3684e678
RetroJack <31865555+RetroJack@users.noreply.github.com>
2018-07-01 18:01:31 -0500

    Init
    Just added the basic stuff and using a class structure for funsies might remove later. It is easier to remove it than to add it. Currently intending it to be the main driving file for the bot

---
+++ discordObj.py
@@ -0,0 +1,15 @@
+import Lootbot
+import discord
+from discord.ext import commands
+
+class discordDriver:
+
+    botDescription = 'Lootbot is intended as a tool for dungeon masters to use in addition to using loot tables'
+    bot = commands.Bot(command_prefix = '~', description = botDescription)
+    def __init__(self):
+        pass
+
+    def 
+
+if __name__ == "__main__":
+    pass
