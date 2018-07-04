6b0f01f2beddcde5abfc24000fd2ea7741474d81
RetroJack <31865555+RetroJack@users.noreply.github.com>
2018-07-04 13:59:02 -0500

    Making corrections based on API doc
    Making some changes and correcting as I become more familiar with the documentation. Bot.Command is what we need instead of the unholy mess I was attempting before

--- discordObj.py
+++ discordObj.py
@@ -1,23 +1,37 @@
-import Lootbot
-import discord
+import Lootbot, discord, json, atexit, logging
 from discord.ext import commands
 
-class discordDriver:
 
-    botDescription = 'Lootbot is intended as a tool for dungeon masters to use in addition to using loot tables'
-    bot = commands.Bot(command_prefix = '~', description = botDescription)
-    def __init__(self):
-        pass
+botDescription = 'Lootbot is intended as a tool for dungeon masters to use in addition to using loot tables'
+lootbot = commands.Bot(command_prefix = '@lootbot', description = botDescription)
+  
+@lootbot.event
+async def on_ready():
+    logger.info('Logged in as\nUsername:'+ bot.user.name + '\nID:' + '\n')
 
-    def readIN():
-        return "user input as string"
 
-class threadHandler:
-    #In the offchance that threads are used
+@lootbot.listen()
+async def read_in():
+    return "user input as string"
+
+@lootbot.command()
+async def roll(ctx):
+    pass
+
+def on_exit():
     pass
 
 if __name__ == "__main__":
-    print("----------------------\n","LootBot","----------------------\n")
-    print("checking config \n")#check for user settings if it exists confirm info else ask for it
-    print("initializing \n")#set object variables here give fail message if there is an issue
-    print("successfully started LootBot")
+    logging.basicConfig(level = logging.INFO)
+    logger = logging.getLogger(__name__)
+    try:
+        print("----------------------\n","LootBot","----------------------\n")
+        logger.info('Checking Config')
+        print("checking config \n")#check for user settings if it exists confirm info else ask for it
+        print("initializing \n")#set object variables here give fail message if there is an issue
+        print("successfully started LootBot")
+        atexit.register(on_exit())
+    except ValueError as error:
+        pass
+    except SystemExit as error:
+        pass
